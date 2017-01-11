09cab38d73687f1ce8a6cf1e4e1eda7df92580d9
karthiky7cs1212 <karthiky7cs1212@gmail.com>
2017-01-11 00:50:10 -0500

    Update Store.java

--- src/tutorial/store/Store.java
+++ src/tutorial/store/Store.java
@@ -1,32 +1,33 @@
-package tutorial.store;
+
 
 import java.util.*;
 
 public class Store {
-
-	private String[] items;
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		// get the input for number of Items:
-		System.out.println("Enter The number of Items:");
-		int no_of_items = sc.nextInt();
-
-		// TODO Modify the following code to read item name, description and price and store details in a class called Item.
-		
-		// TODO Store item in a list of type Item instead of array
-		
-		// store into String array
-		String items[] = new String[no_of_items];
-		System.out.println("Enter Item Names: ");
-
-		for (int i = 0; i < items.length; i++) {
-			items[i] = sc.next();
+	 
+	 public static void main(String[] args) {
+	 	Scanner sc = new Scanner(System.in);
+	 
+	 	// get the input for number of Items:
+	 	System.out.println("Enter The number of Items:");
+	 	int no_of_items = Integer.parseInt(sc.nextLine());
+	 	
+	 	//Reading The Items
+	 	
+	 	List<Item> list = new ArrayList<Item>();	
+	 	
+	 	for(int i=0;i<no_of_items;i++) {
+	 		
+	 		String itemName = sc.nextLine();
+	 		String description = sc.nextLine();
+	 		int price = Integer.parseInt(sc.nextLine());
+	 		Item Ite = new Item(itemName,description,price);
+	 		list.add(Ite);
+	 			 		
+	 	}
+	 	
+	 	for(Item it:list)
+	 	{
+	 	 System.out.println(it.toString());
 		}
-
-		for (String item : items) {
-			System.out.println(item);
-		}
-	}
+	 }
 }

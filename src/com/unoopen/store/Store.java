d99b1ef72caeede618736e17afa788866b177db5
geek <me@lantr.com>
2017-01-11 22:40:27 +0800

    Add todo

--- src/com/unoopen/store/Store.java
+++ src/com/unoopen/store/Store.java
@@ -7,6 +7,17 @@ import java.util.Scanner;
 public class Store {
 
 	public static void main(String[] args) {
+		// TODO READ items from a file.
+		// Create a class which reads from a file and add the items
+		
+		// Assume first line of file is header
+		// File columns are separated by tab space
+		
+		// Example:
+		// Name	Description	Price
+		// Item1	Pen	10
+		// Item2	Book 20
+		
 		Scanner sc = new Scanner(System.in);
 
 		// get the input for number of Items:
@@ -17,8 +28,11 @@ public class Store {
 		List<Item> list = new ArrayList<Item>();
 
 		for (int i = 0; i < noOfItems; i++) {
+			System.out.println("Item Name:");
 			String itemName = sc.nextLine();
+			System.out.println("Item Description:");
 			String description = sc.nextLine();
+			System.out.println("Item Price:");
 			int price = Integer.parseInt(sc.nextLine());
 			Item Item = new Item(itemName, description, price);
 			list.add(Item);

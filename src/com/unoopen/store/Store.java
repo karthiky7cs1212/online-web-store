22261a75ef51473550be2c63e4a3d0c61f075c49
scoda <scoda@DESKTOP-8G0P3AC>
2017-01-15 21:14:15 -0500

    modified store

--- src/com/unoopen/store/Store.java
+++ src/com/unoopen/store/Store.java
@@ -1,43 +1,36 @@
 package com.unoopen.store;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Store {
-
-	public static void main(String[] args) {
-		// TODO READ items from a file.
-		// Create a class which reads from a file and add the items
+   
+	private static final String FILENAME = "C:\\Users\\scoda\\Desktop\\items.txt";
+	public static void main(String[] args) throws IOException {
 		
-		// Assume first line of file is header
-		// File columns are separated by tab space
-		
-		// Example:
-		// Name	Description	Price
-		// Item1	Pen	10
-		// Item2	Book 20
-		
-		Scanner sc = new Scanner(System.in);
-
-		// get the input for number of Items:
-		System.out.println("Enter The number of Items:");
-		int noOfItems = Integer.parseInt(sc.nextLine());
-
-		// Reading The Items
+		BufferedReader br = null;
 		List<Item> list = new ArrayList<Item>();
-
-		for (int i = 0; i < noOfItems; i++) {
-			System.out.println("Item Name:");
-			String itemName = sc.nextLine();
-			System.out.println("Item Description:");
-			String description = sc.nextLine();
-			System.out.println("Item Price:");
-			int price = Integer.parseInt(sc.nextLine());
-			Item Item = new Item(itemName, description, price);
-			list.add(Item);
+		try {
+			String strLine;
+			 br = new BufferedReader(new FileReader(FILENAME));
+			 br.readLine();
+			 
+			while ((strLine = br.readLine()) != null)   {
+				String[] arr = strLine.split("\\s+"); //Breaking each line into array of words
+				Item item = new Item(arr[0],arr[1],Integer.parseInt(arr[2]));
+				list.add(item);
+				}
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
+		
 		for (Item item : list) {
 			System.out.println(item);
 		}
